b5d461146077765fb7961733ff42c01728704470
Felipe Cardoso Resende <felipecardoso.fcr@gmail.com>
2014-03-22 16:50:48 -0300

    Added exercises 1.1 and 1.2 and created directory Aula1

---
+++ Aula1/ex1.1.sh
@@ -0,0 +1,11 @@
+a partir da pasta vagrant:
+
+mkdir praticas_desenvolvimento_software
+ln -s praticas_desenvolvimento_software devsoft
+cd praticas_desenvolvimento_software
+mkdir Felipe
+ln -s Felipe Nomade
+ln -s Felipe 8041585
+cd Felipe
+mkdir aula01
+vim ex1.1.sh
---
+++ Aula1/ex1.2.sh
@@ -0,0 +1,2 @@
+wget www.tlpd.org/LDP/intro-linux/html/intro-linux.html
+grep "linux" intro-linux.html | wc -l > numero_linhas.txt
