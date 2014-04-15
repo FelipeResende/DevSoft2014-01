7cf700fa0341f0f4ff5c1a057ad418e00058daf1
Felipe Cardoso Resende <felipecardoso.fcr@gmail.com>
2014-04-15 18:28:10 -0300

    exercise 5.4.pl

--- Aula5/Exercises/ex5.4.pl
+++ Aula5/Exercises/ex5.4.pl
@@ -2,10 +2,10 @@
 %% Implementar fatorial em Prolog.
 fat(0, 1).
 fat(1, 1).
-fat(N, NF):-
-	A is N, B is N - 1,
-	fat (B, BF),
-	NF is A * BF.
-?- fat(10, X)
+fat(N, NF) :-
+	B is N - 1,
+	fat(B, BF),
+	NF is N * BF.
+?- fat(10, X).
 %% Exemplo de query:
 %% ?- fat(10, X)
